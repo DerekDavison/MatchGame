f2966b5bc0768031fd367f53a43d1b599f1e2005
Zoltan Kovacs <zoltan321@gmail.com>
2012-11-19 14:37:58 -0800

    Testing Push
    Testing Push

--- src/com/example/matchgame/GameOver.java
+++ src/com/example/matchgame/GameOver.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class GameOver extends Activity {
+public class GameOver extends Activity 
+{
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_over);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
         getMenuInflater().inflate(R.menu.game_over, menu);
         return true;
     }
