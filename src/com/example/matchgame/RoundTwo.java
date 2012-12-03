d563f7e8b8c3691b230c8d447896a566ed7865a4
Derek Davison <derekdavison33@hotmail.com>
2012-12-03 12:37:19 -0800

    little bit of round 2

--- src/com/example/matchgame/RoundTwo.java
+++ src/com/example/matchgame/RoundTwo.java
@@ -5,7 +5,10 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class RoundTwo extends Activity implements OnClickListener
@@ -14,6 +17,9 @@ public class RoundTwo extends Activity implements OnClickListener
 	ImageView imgContinue;
 	int count = 0;
 	TextView hostText;
+	Button submit;
+	EditText ownText;
+	RadioButton select;
 
     @Override
     public void onCreate(Bundle savedInstanceState) 
@@ -23,6 +29,9 @@ public class RoundTwo extends Activity implements OnClickListener
         
         imgContinue = (ImageView)findViewById(R.id.imgContinue);
         hostText = (TextView)findViewById(R.id.txtQuestion);
+        submit = (Button)findViewById(R.id.btnSubmit);
+        ownText = (EditText)findViewById(R.id.response);
+        select = (RadioButton)findViewById(R.id.rdbOther);
     }
         
         public void onClick(View v)
@@ -44,17 +53,24 @@ public class RoundTwo extends Activity implements OnClickListener
     	    				hostText.setText("Our player may choose to take one of their answers or select their own.");
     	    				break;
     	    			case 4:
+    	    				hostText.setText("If the selected answer matches one of the corresponding prizes, our champion will have that prize added to their total.");
+    	    			case 5:
     	    				hostText.setText("Now let's get this round started!");
     	    				break;
-    	    			case 5:
+    	    			case 6:
     	    				//load question
-    	    				imgContinue.setVisibility(0);
+    	    				imgContinue.setVisibility(View.INVISIBLE);
+    	    				break;	
+    	    			default:
     	    				break;
     	    		}
+    	    		break;
+    	    	case R.id.rdbOther:
+    	    		ownText.setVisibility(View.VISIBLE);
+    	    		break;
+    	    	case R.id.btnSubmit:
     	    		
-    	    		break;
-    	    	default:
-    	    		break;
+    	    	
         	}
     	}
     	    			
